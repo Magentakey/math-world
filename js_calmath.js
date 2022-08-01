e1afc0914cc74fff4fe2fb6b17ce587eaa6b0324
Magentakey <109955875+Magentakey@users.noreply.github.com>
2022-08-02 06:15:35 +0700

    Update mathjax

--- js_calmath.js
+++ js_calmath.js
@@ -15,10 +15,11 @@ let c = Number(inputc_box3.value)
 	`
 	ax<sup>2</sup> + bx + c = 0<br>
 	a = ${a} , b = ${b} , c = ${c}<br><br>
-	x1+x2 = -b/a <br>
-	x1+x2 = -(${b}/${a})<br>
-	x1+x2 = ${-(b/a)}
+	\\( x1+x2 \\) = \\( \\frac{-b}{a} \\) <br>
+	\\( x1+x2 \\) = \\( \\frac{${-b}}{${a}} \\)<br>
+	\\( x1+x2 \\) = \\( ${-b/a} \\)
 	`;
+	MathJax.typeset();
 }
 
 function hasil_dua_box3(){
@@ -30,10 +31,11 @@ let c = Number(inputc_box3.value)
 	`
 	ax<sup>2</sup> + bx + c = 0<br>
 	a = ${a} , b = ${b} , c = ${c}<br><br>
-	x1.x2 = c/a <br>
-	x1.x2 = ${c}/${a}<br>
-	x1.x2 = ${c/a}
+	\\( x1.x2 \\) = \\( \\frac{c}{a} \\)<br>
+	\\( x1.x2 \\) = \\( \\frac{${c}}{${a}} \\)<br>
+	\\( x1.x2 \\) = \\( ${c/a} \\)
 	`;
+	MathJax.typeset();
 }
 
 function hasil_tiga_box3(){
@@ -45,11 +47,12 @@ let c = Number(inputc_box3.value)
 	`
 	ax<sup>2</sup> + bx + c = 0<br>
 	a = ${a} , b = ${b} , c = ${c}<br><br>
-	x1<sup>2</sup>+x2<sup>2</sup> = (x1+x2)<sup>2</sup> - 2(x1.x2) <br>
-	x1<sup>2</sup>+x2<sup>2</sup> = ${-(b/a)}<sup>2</sup> - 2.${c/a}<br>
-	x1<sup>2</sup>+x2<sup>2</sup> = ${(-(b/a)) ** 2} + ${-2*c/a}<br>
-	x1<sup>2</sup>+x2<sup>2</sup> = ${((-(b/a)) ** 2) + (-2*c/a)}
+	\\( x1^2+x2^2 \\) = \\( (x1+x2)^2 - 2(x1.x2) \\)<br>
+	\\( x1^2+x2^2 \\) = \\( ${-b/a}^2 - 2.${c/a} \\)<br>
+	\\( x1^2+x2^2 \\) = \\( ${(-b/a) ** 2} + ${-2*c/a} \\)<br>
+	\\( x1^2+x2^2 \\) = \\(${((-(b/a)) ** 2) + (-2*c/a)} \\)
 	`;
+	MathJax.typeset();
 }
 
 function hasil_empat_box3(){
@@ -61,10 +64,11 @@ let c = Number(inputc_box3.value)
 	`
 	ax<sup>2</sup> + bx + c = 0<br>
 	a = ${a} , b = ${b} , c = ${c}<br><br>
-	(1/x1)+(1/x2) = (x1+x2)/(x1.x2) <br>
-	(1/x1)+(1/x2) = ${-(b/a)}/${c/a}<br>
-	(1/x1)+(1/x2) = ${(-(b/a))/(c/a)}
+	\\( \\frac{1}{x1} + \\frac{1}{x2} \\) = \\( \\frac{x1+x2}{x1.x2} \\)<br>
+	\\( \\frac{1}{x1} + \\frac{1}{x2} \\) = \\( \\frac{${-(b/a)}}{${c/a}} \\)<br>
+	\\( \\frac{1}{x1} + \\frac{1}{x2} \\) = \\(${(-(b/a))/(c/a)} \\)
 	`;
+	MathJax.typeset();
 }
 
 function hasil_lima_box3(){
@@ -76,10 +80,11 @@ let c = Number(inputc_box3.value)
 	`
 	ax<sup>2</sup> + bx + c = 0<br>
 	a = ${a} , b = ${b} , c = ${c}<br><br>
-	(x1/x2)+(x2/x1) = (x1<sup>2</sup>+x2<sup>2</sup>)/(x1.x2) <br>
-	(x1/x2)+(x2/x1) = ${((-(b/a)) ** 2) + (-2*c/a)}/${c/a}<br>
-	(x1/x2)+(x2/x1) = ${(((-(b/a)) ** 2) + (-2*c/a))/(c/a)}
+	\\( \\frac{x1}{x2}+\\frac{x2}{x1} \\) = \\( \\frac{x1^2+x2^2}{x1.x2} \\)<br>
+	\\( \\frac{x1}{x2}+\\frac{x2}{x1} \\) = \\( \\frac{${((-(b/a)) ** 2) + (-2*c/a)}}{${c/a}} \\)<br>
+	\\( \\frac{x1}{x2}+\\frac{x2}{x1} \\) = \\(${(((-(b/a)) ** 2) + (-2*c/a))/(c/a)} \\)
 	`;
+	MathJax.typeset();
 }
 ////////////////////////////////////////////////////////////////
 // box2
@@ -99,66 +104,85 @@ function hasilfaktor_box2(){
 
 if (diskriminan > 0) {
 	if (diskriminan % akar_diskriminan == 0){
-		return hasil_box2.innerHTML = 
+		hasil_box2.innerHTML = 
 	`
-	x<sub>1,2</sub> = (-b &#177; &#8730; (b<sup>2</sup> - 4ac)) / 2a<br>
-	x<sub>1,2</sub> = (-(${b_box2}) &#177; &#8730; ((${b_box2})<sup>2</sup> - 4.(${a_box2}).(${c_box2}))) / 2.(${a_box2})<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; &#8730; (${b_box2 ** 2}) + (${-4 * a_box2 * c_box2}))) / ${2 * a_box2}<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; &#8730; (${(b_box2 ** 2) + (-4 * a_box2 * c_box2)})) / ${2 * a_box2}<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; ${akar_diskriminan}) / ${2 * a_box2}<br>
+	x<sub>1,2</sub> = \\( \\frac{-b \\pm \\sqrt{b^2- 4ac}}{2a} \\) <br>
+	x<sub>1,2</sub> = \\( \\frac{-(${b_box2}) \\pm \\sqrt{(${b_box2})^2 - 4.(${a_box2}).(${c_box2})}}{2.(${a_box2})} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm \\sqrt{(${b_box2 ** 2}) + (${-4 * a_box2 * c_box2})}}{${2 * a_box2}} \\) <br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm \\sqrt{(${(b_box2 ** 2) + (-4 * a_box2 * c_box2)})}}{${2 * a_box2}} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm ${akar_diskriminan}}{${2 * a_box2}} \\) <br>
 	<br>
-	x<sub>1</sub> = (${-b_box2} + ${akar_diskriminan}) / ${2 * a_box2}<br>
+	x<sub>1</sub> = \\( \\frac{${-b_box2} + ${akar_diskriminan}}{${2 * a_box2}} \\) <br>
 	x<sub>1</sub> = ${-b_box2 + akar_diskriminan} / ${2 * a_box2}<br>
-	x<sub>1</sub> = ${(-b_box2 + akar_diskriminan) / (2 * a_box2)}<br>
+	x<sub>1</sub> = \\( \\frac{${-b_box2 + akar_diskriminan}}{${2 * a_box2}} \\)<br>
+	x<sub>1</sub> = \\( ${(-b_box2 + akar_diskriminan) / (2 * a_box2)} \\)<br>
 	<br>
-	x<sub>2</sub> = (${-b_box2} - ${akar_diskriminan}) / ${2 * a_box2}<br>
-	x<sub>2</sub> = ${-b_box2 - akar_diskriminan} / ${2 * a_box2}<br>
-	x<sub>2</sub> = ${(-b_box2 - akar_diskriminan) / (2 * a_box2)}<br>
-	`}
+	x<sub>2</sub> = \\( \\frac{${-b_box2} - ${akar_diskriminan}}{${2 * a_box2}} \\)<br>
+	x<sub>2</sub> = \\( \\frac{${-b_box2 - akar_diskriminan}}{${2 * a_box2}} \\)<br>
+	x<sub>2</sub> = \\( ${(-b_box2 - akar_diskriminan) / (2 * a_box2)} \\)<br>
+	`
+	MathJax.typeset();
+	}
 	else if (diskriminan % akar_diskriminan != 0) {
-			return hasil_box2.innerHTML = 
+			hasil_box2.innerHTML = 
 	`
-	x<sub>1,2</sub> = (-b &#177; &#8730; (b<sup>2</sup> - 4ac)) / 2a<br>
-	x<sub>1,2</sub> = (-(${b_box2}) &#177; &#8730; ((${b_box2})<sup>2</sup> - 4.(${a_box2}).(${c_box2}))) / 2.(${a_box2})<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; &#8730; (${b_box2 ** 2} + (${-4 * a_box2 * c_box2}))) / ${2 * a_box2}<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; &#8730; (${(b_box2 ** 2) + (-4 * a_box2 * c_box2)})) / ${2 * a_box2}<br>
+	x<sub>1,2</sub> = \\( \\frac{-b \\pm \\sqrt{(b^2 - 4ac))}}{2a} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{-(${b_box2}) \\pm \\sqrt{(${b_box2})^2 - 4.(${a_box2}).(${c_box2})}}{2.(${a_box2})} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm \\sqrt{${b_box2 ** 2} + (${-4 * a_box2 * c_box2})}}{${2 * a_box2}} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm \\sqrt{(${(b_box2 ** 2) + (-4 * a_box2 * c_box2)})}}{${2 * a_box2}} \\)<br>
 	<br>
-	x<sub>1</sub> = (${-b_box2} + &#8730; (${(b_box2 ** 2) + (-4 * a_box2 * c_box2)})) / ${2 * a_box2}<br>
+	x<sub>1</sub> = \\( \\frac{${-b_box2} + \\sqrt{(${(b_box2 ** 2) + (-4 * a_box2 * c_box2)})}}{${2 * a_box2}} \\)<br>
 	<br>
-	x<sub>2</sub> = (${-b_box2} - &#8730; (${(b_box2 ** 2) + (-4 * a_box2 * c_box2)})) / ${2 * a_box2}<br>
+	x<sub>2</sub> = \\( \\frac{${-b_box2} - \\sqrt{(${(b_box2 ** 2) + (-4 * a_box2 * c_box2)})}}{${2 * a_box2}} \\)<br>
 	`
+	MathJax.typeset();
 	}
 }
 else if (diskriminan == 0){
-		return hasil_box2.innerHTML = 
+		hasil_box2.innerHTML = 
 	`
-	x<sub>1,2</sub> = (-b &#177; &#8730; (b<sup>2</sup> - 4ac)) / 2a<br>
-	x<sub>1,2</sub> = (-(${b_box2}) &#177; &#8730; ((${b_box2})<sup>2</sup> - 4.(${a_box2}).(${c_box2}))) / 2.(${a_box2})<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; &#8730; (${b_box2 ** 2} + (${-4 * a_box2 * c_box2}))) / ${2 * a_box2}<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; &#8730; (${(b_box2 ** 2) + (-4 * a_box2 * c_box2)})) / ${2 * a_box2}<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; ${akar_diskriminan}) / ${2 * a_box2}   <<<  hanya memiliki 1 nilai<br>
-	x = ${-b_box2} / ${2 * a_box2}<br>
+	x<sub>1,2</sub> = \\( \\frac{-b \\pm \\sqrt{(b^2 - 4ac))}}{2a} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{-(${b_box2}) \\pm \\sqrt{(${b_box2})^2 - 4.(${a_box2}).(${c_box2})}}{2.(${a_box2})} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm \\sqrt{${b_box2 ** 2} + (${-4 * a_box2 * c_box2})}}{${2 * a_box2}} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm \\sqrt{(${(b_box2 ** 2) + (-4 * a_box2 * c_box2)})}}{${2 * a_box2}} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm ${akar_diskriminan}}{${2 * a_box2}} \\)   <<<  hanya memiliki 1 nilai<br>
+	x = \\( \\frac{${-b_box2}}{${2 * a_box2}} \\)<br>
 	x = ${-b_box2 / (2 * a_box2)}<br>
 	`
+	MathJax.typeset();
 }
 else if (diskriminan < 0){
 	if (khususmin_diskriminan % khususmin_akar_diskriminan == 0) {
-		return hasil_box2.innerHTML = 
+		hasil_box2.innerHTML = 
 	`
-	x<sub>1,2</sub> = (-b &#177; &#8730; (b<sup>2</sup> - 4ac)) / 2a<br>
-	x<sub>1,2</sub> = (-(${b_box2}) &#177; &#8730; ((${b_box2})<sup>2</sup> - 4.(${a_box2}).(${c_box2}))) / 2.(${a_box2})<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; &#8730; (${b_box2 ** 2} + (${-4 * a_box2 * c_box2}))) / ${2 * a_box2}<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; &#8730; (${(b_box2 ** 2) + (-4 * a_box2 * c_box2)})) / ${2 * a_box2}<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; ${khususmin_akar_diskriminan}i) / ${2 * a_box2} <<< tidak ada penyelesaian<br>
-	`}
+	x<sub>1,2</sub> = \\( \\frac{-b \\pm \\sqrt{(b^2 - 4ac))}}{2a} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{-(${b_box2}) \\pm \\sqrt{(${b_box2})^2 - 4.(${a_box2}).(${c_box2})}}{2.(${a_box2})} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm \\sqrt{${b_box2 ** 2} + (${-4 * a_box2 * c_box2})}}{${2 * a_box2}} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm \\sqrt{${(b_box2 ** 2) + (-4 * a_box2 * c_box2)}}}{${2 * a_box2}} \\) <<< tidak ada penyelesaian,karena nilai akar selalu positif <br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm ${khususmin_akar_diskriminan}i}{${2 * a_box2}} \\) <<< jika ingin dilanjutkan menggunakan bilangan imajiner<br>
+	<br>
+	x<sub>1</sub> = \\( \\frac{${-b_box2} + ${khususmin_akar_diskriminan}i}{${2 * a_box2}} \\) <br>
+	x<sub>1</sub> = \\( ${-b_box2 / (2 * a_box2)} + ${khususmin_akar_diskriminan / (2 * a_box2)}i \\) 
+	<br><br>
+	x<sub>2</sub> = \\( \\frac{${-b_box2} - ${khususmin_akar_diskriminan}i}{${2 * a_box2}} \\)<br>
+	x<sub>2</sub> = \\( ${(-b_box2 / (2 * a_box2))} + ${(-khususmin_akar_diskriminan / (2 * a_box2))}i \\)
+	`
+	MathJax.typeset();
+	}
 	else if(khususmin_diskriminan % khususmin_akar_diskriminan != 0 ){
-			return hasil_box2.innerHTML = 
+			hasil_box2.innerHTML = 
 	`
-	x<sub>1,2</sub> = (-b &#177; &#8730; (b<sup>2</sup> - 4ac)) / 2a<br>
-	x<sub>1,2</sub> = (-(${b_box2}) &#177; &#8730; ((${b_box2})<sup>2</sup> - 4.(${a_box2}).(${c_box2}))) / 2.(${a_box2})<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; &#8730; (${b_box2 ** 2} + (${-4 * a_box2 * c_box2}))) / ${2 * a_box2}<br>
-	x<sub>1,2</sub> = (${-b_box2} &#177; &#8730; (${diskriminan})) / ${2 * a_box2} <<< tidak ada penyelesaian<br>
+	x<sub>1,2</sub> = \\( \\frac{-b \\pm \\sqrt{(b^2 - 4ac))}}{2a} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{-(${b_box2}) \\pm \\sqrt{(${b_box2})^2 - 4.(${a_box2}).(${c_box2})}}{2.(${a_box2})} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm \\sqrt{${b_box2 ** 2} + (${-4 * a_box2 * c_box2})}}{${2 * a_box2}} \\)<br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm \\sqrt{${diskriminan}}}{${2 * a_box2}} \\) <<< tidak ada penyelesaian,karna nilai akar selalu positif<br>
+	x<sub>1,2</sub> = \\( \\frac{${-b_box2} \\pm \\sqrt{${diskriminan}}}{${2 * a_box2}} \\) <<< jika tetap ingin dilanjutkan
+	<br><br>
+	x<sub>1</sub> = \\( \\frac{${-b_box2} + \\sqrt{${diskriminan}}}{${2 * a_box2}} \\)
+	<br><br>
+	x<sub>2</sub> = \\( \\frac{${-b_box2} - \\sqrt{${diskriminan}}}{${2 * a_box2}} \\)
 	`
+	MathJax.typeset();
 	}
 }}
 ////////////////////////////////////////////////////////////////
